fdcb8fa1f048a75cd5a23c3b1680bb0067361f0e
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2022-07-30 22:50:22 +0200

    Create simple component NetflixApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import NetflixApp from "./components/NetflixApp";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My first commit here with Github desktop
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <NetflixApp />
 }
 
 export default App;
