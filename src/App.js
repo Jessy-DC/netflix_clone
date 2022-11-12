167c56c3761f87cb65914e449088a469131a6161
Jessy CHARLOT <50245217+Jessy-DC@users.noreply.github.com>
2022-11-12 23:04:54 +0100

    Remove unused files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import * as React from 'react'
-import './mocks'
-import * as authNetflix from './utils/authNetflixProvider'
 import {createTheme, ThemeProvider} from '@mui/material/styles'
 import {AuthApp} from './AuthApp'
-import {UnauthApp} from './UnauthApp'
 
 const theme = createTheme({
     palette: {
